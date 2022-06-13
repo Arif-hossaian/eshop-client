0c220d3c295360f8497e829eb150bf9744fa63cf
Arif-hossaian <arifhossain42136@gmail.com>
2022-06-13 13:34:20 +0600

    fix errors

--- pages/cart/checkout.tsx
+++ pages/cart/checkout.tsx
@@ -5,16 +5,18 @@ import CheckoutItems from '../../components/checkout/items';
 import { RootState } from 'store';
 
 const CheckoutPage = () => {
-
   const priceTotal = useSelector((state: RootState) => {
     const cartItems = state.cart.cartItems;
     let totalPrice = 0;
-    if(cartItems.length > 0) {
-      cartItems.map(item => totalPrice += item.price * item.count);
+    if (cartItems.length > 0) {
+      cartItems.map(
+        (item: { price: number; count: number }) =>
+          (totalPrice += item.price * item.count)
+      );
     }
 
     return totalPrice;
-  })
+  });
 
   return (
     <Layout>
@@ -29,7 +31,9 @@ const CheckoutPage = () => {
             <div className="checkout__col-6">
               <div className="checkout__btns">
                 <button className="btn btn--rounded btn--yellow">Log in</button>
-                <button className="btn btn--rounded btn--border">Sign up</button>
+                <button className="btn btn--rounded btn--border">
+                  Sign up
+                </button>
               </div>
 
               <div className="block">
@@ -37,37 +41,65 @@ const CheckoutPage = () => {
                 <form className="form">
                   <div className="form__input-row form__input-row--two">
                     <div className="form__col">
-                      <input className="form__input form__input--sm" type="text" placeholder="Email" />
+                      <input
+                        className="form__input form__input--sm"
+                        type="text"
+                        placeholder="Email"
+                      />
                     </div>
 
                     <div className="form__col">
-                      <input className="form__input form__input--sm" type="text" placeholder="Address" />
-                    </div>
-                  </div>
-                  
-                  <div className="form__input-row form__input-row--two">
-                    <div className="form__col">
-                      <input className="form__input form__input--sm" type="text" placeholder="First name" />
-                    </div>
-
-                    <div className="form__col">
-                      <input className="form__input form__input--sm" type="text" placeholder="City" />
-                    </div>
-                  </div>
-                  
-                  <div className="form__input-row form__input-row--two">
-                    <div className="form__col">
-                      <input className="form__input form__input--sm" type="text" placeholder="Last name" />
-                    </div>
-
-                    <div className="form__col">
-                      <input className="form__input form__input--sm" type="text" placeholder="Postal code / ZIP" />
+                      <input
+                        className="form__input form__input--sm"
+                        type="text"
+                        placeholder="Address"
+                      />
                     </div>
                   </div>
 
                   <div className="form__input-row form__input-row--two">
                     <div className="form__col">
-                      <input className="form__input form__input--sm" type="text" placeholder="Phone number" />
+                      <input
+                        className="form__input form__input--sm"
+                        type="text"
+                        placeholder="First name"
+                      />
+                    </div>
+
+                    <div className="form__col">
+                      <input
+                        className="form__input form__input--sm"
+                        type="text"
+                        placeholder="City"
+                      />
+                    </div>
+                  </div>
+
+                  <div className="form__input-row form__input-row--two">
+                    <div className="form__col">
+                      <input
+                        className="form__input form__input--sm"
+                        type="text"
+                        placeholder="Last name"
+                      />
+                    </div>
+
+                    <div className="form__col">
+                      <input
+                        className="form__input form__input--sm"
+                        type="text"
+                        placeholder="Postal code / ZIP"
+                      />
+                    </div>
+                  </div>
+
+                  <div className="form__input-row form__input-row--two">
+                    <div className="form__col">
+                      <input
+                        className="form__input form__input--sm"
+                        type="text"
+                        placeholder="Phone number"
+                      />
                     </div>
 
                     <div className="form__col">
@@ -82,7 +114,7 @@ const CheckoutPage = () => {
                 </form>
               </div>
             </div>
-            
+
             <div className="checkout__col-4">
               <div className="block">
                 <h3 className="block__title">Payment method</h3>
@@ -107,7 +139,7 @@ const CheckoutPage = () => {
                   </li>
                 </ul>
               </div>
-              
+
               <div className="block">
                 <h3 className="block__title">Delivery method</h3>
                 <ul className="round-options round-options--two">
@@ -130,12 +162,12 @@ const CheckoutPage = () => {
                 </ul>
               </div>
             </div>
-            
+
             <div className="checkout__col-2">
               <div className="block">
                 <h3 className="block__title">Your cart</h3>
                 <CheckoutItems />
-                
+
                 <div className="checkout-total">
                   <p>Total cost</p>
                   <h3>${priceTotal}</h3>
@@ -143,19 +175,24 @@ const CheckoutPage = () => {
               </div>
             </div>
           </div>
-          
+
           <div className="cart-actions cart-actions--checkout">
-            <a href="/cart" className="cart__btn-back"><i className="icon-left"></i> Back</a>
+            <a href="/cart" className="cart__btn-back">
+              <i className="icon-left"></i> Back
+            </a>
             <div className="cart-actions__items-wrapper">
-              <button type="button" className="btn btn--rounded btn--border">Continue shopping</button>
-              <button type="button" className="btn btn--rounded btn--yellow">Proceed to payment</button>
+              <button type="button" className="btn btn--rounded btn--border">
+                Continue shopping
+              </button>
+              <button type="button" className="btn btn--rounded btn--yellow">
+                Proceed to payment
+              </button>
             </div>
           </div>
         </div>
       </section>
     </Layout>
-  )
+  );
 };
 
-  
-export default CheckoutPage
+export default CheckoutPage;
